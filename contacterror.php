64c56599063a2a1294a8ea155eb69463bb722e0f
cestridge <cestridge@gmail.com>
2014-10-02 14:51:43 -0400

    adding contact form functionality

---
+++ contacterror.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Strict//EN">
+
+<head>
+	<meta charset="UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link type="text/plain" rel="author" href="assets/inc/humans.txt" />
+    <link rel="shortcut icon" type="image/ico" href="favicon.ico" />
+
+    <title>AMMC</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css"/>
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css"/>
+</head>
+
+<body>
+<header class="typography">
+    <div class="row headerTop">
+        <div class="six column right">
+            <div class="callUs headingQuiet">
+                <h3><span>toll free</span><a class="call" href="tel:+18446552662">(844) 655-AMMC</a></h3>
+                <div class="callUsCopy headingLoud"><h5><strong>Call Us When You Are Ready to </strong></h5></div>
+                <div class="callList">
+                    <span class="animated"><h4 class="headingYellow"><strong><em>Talk</em></strong></h4></span>
+                    <span class="animated"><h4 class="headingYellow"><strong><em>Conceptualize</em></strong></h4></span>
+                    <span class="animated"><h4 class="headingYellow"><strong><em>Design</em></strong></h4></span>
+                    <span class="animated"><h4 class="headingYellow"><strong><em>Engineer</em></strong></h4></span>
+                </div>
+            </div>
+        </div>
+        <div class="four column">
+            <div class="logo"><img src="assets/images/ammc_logo.png"></div>
+        </div>
+    </div>
+    <div class="row titleRow">
+        <div class="headline-container nine column push halfCol title centerText headingLoud">
+            <p class="headingQuiet intro-headline">Error</p>
+            <h1 id="main-headline" class="upper">Sorry, your message failed to send.</h1>
+            <h1 id="sub-headline" class="upper"><span class="headingYellow">Try giving us a call instead.</span></h1>
+            <!-- <p class="headingQuiet exit-headline">We have a new look and invigorated focus, but we continue to provide the same value-added engineering services that we always have.</p> -->
+            <!-- <div id="quoteButton" class="bigTopMargin yellowButton">I’d  Like to TALK about an APPLICATION</div> -->
+            <a href="index.html"><div class="bigTopMargin yellowButton">Back to AMMC.com</div></a>
+        </div>
+    </div>
+</header>
+</body>
+
+</html>
